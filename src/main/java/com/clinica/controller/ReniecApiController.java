93ae68925dbf5e8e600eaf6609f521ec1bab63f4
Chrysr17 <chrisrasaro@gmail.com>
2025-12-02 12:51:09 -0500

    ReniecController

---
+++ src/main/java/com/clinica/controller/ReniecApiController.java
@@ -0,0 +1,36 @@
+package com.clinica.controller;
+
+import com.clinica.dto.ReniecResponse;
+import com.clinica.service.impl.ReniecService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ReniecApiController {
+    @Autowired
+    private ReniecService reniecService;
+
+    @GetMapping("/reniec/{dni}")
+    public ResponseEntity<?> consultarDni(@PathVariable String dni) {
+
+        ReniecResponse datos = reniecService.consultarDni(dni);
+
+        if (datos == null || !datos.isSuccess()) {
+            return ResponseEntity.status(404).body("DNI no encontrado");
+        }
+
+        Map<String, String> resultado = new HashMap<>();
+        resultado.put("nombres", datos.getData().getNombres());
+        resultado.put("apellidos", datos.getData().getApellido_paterno() + " " + datos.getData().getApellido_materno());
+
+        return ResponseEntity.ok(resultado);
+    }
+}
